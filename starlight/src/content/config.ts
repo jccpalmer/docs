ccffb59ceeba1a5db4fa10d7f8006ccc5837a264
JC Palmer <me@jccpalmer.com>
2024-07-10 21:07:41 -0400

    Fix collection schema?

--- starlight/src/content/config.ts
+++ starlight/src/content/config.ts
@@ -1,6 +1,16 @@
 import { defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
+const about = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		tags: z.array(z.string()),
+		image: z.string().optional(),
+	})
+})
+
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
+	'about': about,
 };
