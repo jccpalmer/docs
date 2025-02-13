7d4f869da6708cc8f7fc044e37d6a51d609d57fb
JC Palmer <admin@jccpalmer.com>
2025-02-13 18:19:17 -0500

    Fixed?

---
+++ src/content/config.ts
@@ -0,0 +1,56 @@
+import { z, defineCollection } from 'astro:content';
+import { docsSchema } from '@astrojs/starlight/schema';
+
+const about = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		tags: z.array(z.string()),
+		image: z.string().optional(),
+	})
+})
+
+const docker = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		tags: z.array(z.string()),
+		image: z.string().optional(),
+	})
+})
+
+const software = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		tags: z.array(z.string()),
+		image: z.string().optional(),
+	})
+})
+
+const hardware = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		tags: z.array(z.string()),
+		image: z.string().optional(),
+	})
+})
+
+const gettingStarted = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		tags: z.array(z.string()),
+		image: z.string().optional(),
+	})
+})
+
+export const collections = {
+	docs: defineCollection({ schema: docsSchema() }),
+	'about': about,
+	'docker': docker,
+	'software': software,
+	'hardware': hardware,
+	'getting started': gettingStarted,
+};
